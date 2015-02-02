42f58d5ce100dfa4b21dde407cf80eff1444fdcf
Alexander Ponomarev <alexander996@yandex.ru>
2015-02-02 19:41:56 +0300

    Lear in a loop script modified

--- core/learn_in_a_loop.sh
+++ core/learn_in_a_loop.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 
 while true; do
-    octave -q main.m ~/work/ml/BlindMotionProject/workspace/results/full-trIdle-30-2/out.mat >> out.log; octave -q predict.m result.mat ~/work/ml/BlindMotionProject/workspace/results/cv-trIdle-30-2/out.mat >> log.txt
+    octave -q main.m ~/work/ml/BlindMotionProject/workspace/results/full-trIdle-30-2/out.mat >> out.log;
+    octave -q predict.m result.mat ~/work/ml/BlindMotionProject/workspace/results/cv-trIdle-30-2/out.mat >> log.txt;
+    /home/lamerman/work/ml/BlindMotionProject/code/detector/data_prepaire/predict_events.sh >> log.txt;
+    ~/work/ml/BlindMotionProject/code/detector/data_prepaire/compaire_events.py -a ~/work/ml/BlindMotionProject/Events/20141126/all.json -p ~/work/ml/BlindMotionProject/workspace/events_gen/events.json >> log.txt;
+
     echo "\n\n ***KHRU*** \n\n" >> log.txt
 done
